f8238f6dd8dce0fbe3bd50f965326390ca392977
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-07-01 00:58:08 -0300

    Exception handling - findById

---
+++ src/main/java/com/educandoweb/course/resources/exceptions/StandardError.java
@@ -0,0 +1,65 @@
+package com.educandoweb.course.resources.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.Instant;
+
+public class StandardError extends RuntimeException {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+
+    public StandardError() {}
+
+    public StandardError(Instant timestamp, String message, String path, String error, Integer status) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.path = path;
+        this.error = error;
+        this.status = status;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
